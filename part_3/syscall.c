3fe038a7a3cd2924f218bda0a31b8140c9bffc4d
jpower1113 <jakepower851@yahoo.com>
2025-11-05 19:09:20 -0500

    Create syscall.c

---
+++ part_3/syscall.c
@@ -0,0 +1,39 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/errno.h>
+
+int (*STUB_start_elevator)(void) = NULL;
+int (*STUB_issue_request)(int, int, int) = NULL;
+int (*STUB_stop_elevator)(void) = NULL;
+
+EXPORT_SYMBOL(STUB_start_elevator);
+EXPORT_SYMBOL(STUB_issue_request);
+EXPORT_SYMBOL(STUB_stop_elevator);
+
+SYSCALL_DEFINE0(start_elevator)
+{
+    if (STUB_start_elevator)
+        return STUB_start_elevator();
+    else
+        return -ENOSYS;
+}
+
+SYSCALL_DEFINE3(issue_request, int, start_floor, int, dest_floor, int, type)
+{
+    if (STUB_issue_request)
+        return STUB_issue_request(start_floor, dest_floor, type);
+    else
+        return -ENOSYS;
+}
+
+SYSCALL_DEFINE0(stop_elevator)
+{
+    if (STUB_stop_elevator)
+        return STUB_stop_elevator();
+    else
+        return -ENOSYS;
+}
+
+
